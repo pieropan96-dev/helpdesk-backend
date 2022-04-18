0555731d321783a3f1ec3875fa04fef19abef2b2
Matheus Pieropan <matheus.pieropan@viannasempre.com.br>
2022-04-18 10:10:25 -0300

    Perfil de test criado.

--- src/main/java/com/pieropan/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/pieropan/helpdesk/HelpdeskApplication.java
@@ -1,44 +1,12 @@
 package com.pieropan.helpdesk;
 
-import com.pieropan.helpdesk.dominio.Chamado;
-import com.pieropan.helpdesk.dominio.Cliente;
-import com.pieropan.helpdesk.dominio.Tecnico;
-import com.pieropan.helpdesk.dominio.enums.Perfil;
-import com.pieropan.helpdesk.dominio.enums.Prioridade;
-import com.pieropan.helpdesk.dominio.enums.Status;
-import com.pieropan.helpdesk.repository.ChamadoRepository;
-import com.pieropan.helpdesk.repository.ClienteRepository;
-import com.pieropan.helpdesk.repository.TecnicoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelpdeskApplication implements CommandLineRunner {
-    @Autowired
-    private TecnicoRepository tecnicoRepository;
-
-    @Autowired
-    private ClienteRepository clienteRepostiroy;
-
-    @Autowired
-    private ChamadoRepository chamadoRepository;
+public class HelpdeskApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelpdeskApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        Tecnico tecnico = new Tecnico(null, "Matheus Pieropan", "0192043698", "matheus.2015jf@hotmail.com", "123");
-        tecnico.addPerfil(Perfil.ADMIN);
-        tecnicoRepository.save(tecnico);
-
-        Cliente cliente = new Cliente(null, "José Angusto", "23449456423", "joseaugusto@hotmail.com", "456");
-        clienteRepostiroy.save(cliente);
-
-        Chamado chamado = new Chamado(null, Prioridade.MEDIA, Status.ABERTO, "Chamado 01", "Primeiro chamado", tecnico, cliente);
-        chamadoRepository.save(chamado);
     }
 }
